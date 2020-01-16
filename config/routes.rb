f5da637d151a55128815c14ae2794e6c5a00e5c0
Jesse <jesse.lee.smith@gmail.com>
2020-01-16 13:56:38 -0800

    fixing up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'board_games/index'
-  get 'board_games/show'
-  get 'board_games/new'
-  get 'board_games/edit'
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/new'
-  get 'reviews/edit'
   root 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
@@ -15,5 +7,16 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
     get 'sign_up', to: 'devise/registrations#new'
   end
+
+  resources :user do
+    resources :reviews
+  end
+
+  resources :board_games do
+    resources :reviews, only: [:index]
+  end
+
+  resources :reviews, only: [:show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec.md
+++ spec.md
@@ -13,8 +13,8 @@ Specs:
 - [x] Include login (how e.g. Devise) w/ Devise
 - [x] Include logout (how e.g. Devise) w/ Devise
 - [x] Include third party signup/login (how e.g. Devise/OmniAuth) w/ Devise/Omniauth through GitHub
-- [ ] Include nested resource show or index (URL e.g. users/2/recipes)
-- [ ] Include nested resource "new" form (URL e.g. recipes/1/ingredients/new)
+- [x] Include nested resource show or index (URL e.g. users/2/recipes)
+- [x] Include nested resource "new" form (URL e.g. recipes/1/ingredients/new)
 - [ ] Include form display of validation errors (form URL e.g. /recipes/new)
 
 Confirm:
