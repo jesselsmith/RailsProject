7d3f55f99a20bd02e454aa182e7144d51322f211
Jesse <jesse.lee.smith@gmail.com>
2020-01-18 15:15:42 -0800

    WOrking on BoardGame views

--- app/helpers/board_games_helper.rb
+++ app/helpers/board_games_helper.rb
@@ -1,2 +1,10 @@
 module BoardGamesHelper
+  def get_designer_index(b)
+    Designer.all.find_index(b.object)
+  end
+
+  def is_column_limit?(b)
+    (get_designer_index(b) % (Designer.all.size / 3)) == 0
+  end
+
 end
