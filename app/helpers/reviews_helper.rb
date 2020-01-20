a5f7de55020738e153c5e607f222ec0421d95810
Jesse <jesse.lee.smith@gmail.com>
2020-01-20 00:22:24 -0800

    Fixed helper method for review views to handle nested resource inside of board_game_maker routes

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,6 +2,9 @@ module ReviewsHelper
   def review_subject(review)
     if @nested_resource.is_a? BoardGame
       link_to review.user.email, user_path(review.user)
+    elsif @nested_resource.is_a? BoardGameMaker
+      link_to(review.board_game.title, board_game_path(review.board_game)) +
+        ' review by ' + link_to(review.user.email, user_path(review.user))
     else
       link_to review.board_game.title, board_game_path(review.board_game)
     end
