7b039efd4efe179d98ea9fabd19c6e09f3516259
Jesse <jesse.lee.smith@gmail.com>
2020-01-19 23:15:41 -0800

    Adding to category views and improving RatingAverageable functionality

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,9 @@ class Category < ApplicationRecord
   has_many :reviews, through: :board_games
 
   include RatingAverageable
+
+  def highest_rated_board_game
+    # scope method possibility??
+    
+  end
 end
--- app/models/concerns/rating_averageable.rb
+++ app/models/concerns/rating_averageable.rb
@@ -4,11 +4,22 @@ module RatingAverageable
   def average_rating
     reviews.sum(&:rating).fdiv(reviews.size)
   end
+
+  def sort_board_games_by_average_rating
+    avg_rating_sort(board_games)
+  end
+
+  def highest_rated_board_game
+    sort_board_games_by_average_rating.first
+  end
+
+  def lowest_rated_board_game
+    sort_board_games_by_average_rating.last
+  end
+  
   class_methods do
     def sort_by_average_rating
-      all.sort do |a, b|
-        b.average_rating <=> a.average_rating
-      end
+      avg_rating_sort(all)
     end
 
     def highest_rated
@@ -17,6 +28,14 @@ module RatingAverageable
 
     def lowest_rated
       sort_by_average_rating.last
+    end
+  end
+
+  private
+
+  def avg_rating_sort(collection)
+    collection.sort do |a, b|
+      b.average_rating <=> a.average_rating
     end
   end
 end
