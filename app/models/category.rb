bd98c4bc03620b5ea7acc1d1f98b1969b978d0ec
Jesse <jesse.lee.smith@gmail.com>
2020-01-19 23:26:38 -0800

    Mostly finished with Category views

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,9 +4,4 @@ class Category < ApplicationRecord
   has_many :reviews, through: :board_games
 
   include RatingAverageable
-
-  def highest_rated_board_game
-    # scope method possibility??
-    
-  end
 end
